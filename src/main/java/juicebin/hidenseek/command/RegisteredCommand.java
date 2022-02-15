99d560ac6243efbd6e366d4c61f0de23423bde06
justindnoble <87023781+justindnoble@users.noreply.github.com>
2022-02-15 11:18:26 -0500

    Update command

--- src/main/java/juicebin/hidenseek/command/RegisteredCommand.java
+++ src/main/java/juicebin/hidenseek/command/RegisteredCommand.java
@@ -46,37 +46,23 @@ public abstract class RegisteredCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if (!this.checkSubcommands(sender, command, label, args)) {
+        if (!this.checkSubcommands(sender, command, command.getName(), args)) {
             this.run(sender, command, label, args);
         }
 
         return true;
     }
 
-    private boolean checkSubcommands(CommandSender sender, Command command, String label, String[] args) {
+    private boolean checkSubcommands(CommandSender sender, Command command, String commandName, String[] args) {
         List<Method> methodList = ClassUtils.getMethodsAnnotatedWith(this.getClass(), SubCommand.class)
                 .stream()
                 .filter(method -> {
                     SubCommand subCommand = method.getAnnotation(SubCommand.class);
 
-                    if (command.getName().equals(subCommand.parent()) && args[0].equals(subCommand.name())) {
-                        // If the command name is equal to the subcommand's parent's name
-                        // and the first argument is equal to the name of the subcommand
-                        // e.g. /test target-cmd
-                        // The name of the command, test, is equal to the subcommand's parent's name, test
-                        // The first argument, target-cmd, is equal to the name of the subcommand, target-cmd
-
-
-                    } else if (args[0].equals(subCommand.parent())) {
-                        // If the first argument is equal to the subcommand's parent's name
-                        // e.g. /test cmd target-cmd
-                        // The first argument, cmd, is equal to target-cmd's parent's name, cmd
-                    }
-                    
-                    if (command.getName().equals(subCommand.parent()) && args[0].equals(subCommand.name())) {
+                    if (commandName.equals(subCommand.parent()) && args[0].equals(subCommand.name())) {
                         try {
-                            this.checkSubcommands(sender, command, label, Arrays.copyOfRange(args, 1, args.length + 1));
-                            method.invoke(this, sender, command, label, args);
+                            this.checkSubcommands(sender, command, commandName, Arrays.copyOfRange(args, 1, args.length + 1));
+                            method.invoke(this, sender, command, commandName, args);
                             return true;
                         } catch (IllegalAccessException | InvocationTargetException e) {
                             e.printStackTrace();
